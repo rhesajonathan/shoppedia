3a89988bb7afd65f851a485c87409e14e078ff0b
rhesajonathan <35214884+rhesajonathan@users.noreply.github.com>
2022-08-24 18:30:50 +0700

    add searchbar

---
+++ components/SearchBar.tsx
@@ -0,0 +1,7 @@
+export default function SearchBar () {
+    return(
+        <div className="w-full">
+            <input className="flex p-2 max-w-sm rounded-full w-9/12 border-slate-300 border-2" type="text" placeholder="Search"/>
+        </div>
+    )
+}
